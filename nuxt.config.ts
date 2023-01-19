3cb297e4af0604b1959277640e143d14bad50fd5
gAtrium <pdimetil@gmail.com>
2023-01-19 19:06:16 +0300

    work for the love of god

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-    pages:true,
-    target:"static"
-})
-
 export default {
   target: 'static',
   router: {
-    base: '/udev/'
+    baseURL: '/udev/'
   }
 }
